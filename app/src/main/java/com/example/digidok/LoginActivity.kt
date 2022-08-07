bf5a64d8b3f90ad156c22e82bb0f8b2fe0bbc2dc
eril19 <85010681+eril19@users.noreply.github.com>
2022-08-07 21:59:03 +0700

    Update themes.xml

--- app/src/main/java/com/example/digidok/LoginActivity.kt
+++ app/src/main/java/com/example/digidok/LoginActivity.kt
@@ -1,21 +1,13 @@
 package com.example.digidok
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        supportActionBar?.hide()
-
-        val login = findViewById<Button>(R.id.loginBtn)
-
-        login.setOnClickListener {
-//            startActivity(Intent(this@LoginActivity, DashboardActivity::class.java))
-        }
     }
+
+
 }
