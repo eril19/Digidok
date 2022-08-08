7ca5150c6f289b21c6618152ebe07292584f13fb
eril19 <85010681+eril19@users.noreply.github.com>
2022-08-08 17:02:43 +0700

    progress

--- app/src/main/java/com/example/digidok/LoginActivity.kt
+++ app/src/main/java/com/example/digidok/LoginActivity.kt
@@ -1,13 +1,27 @@
 package com.example.digidok
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
+
+        val loginbtn = findViewById<Button>(R.id.loginBtn)
+
+        loginbtn.setOnClickListener {
+//            val i = Intent(this@LoginActivity, MenuActivity::class.java)
+            startActivity(Intent(this@LoginActivity, MenuActivity::class.java))
+        }
     }
+
+
 
 
 }
