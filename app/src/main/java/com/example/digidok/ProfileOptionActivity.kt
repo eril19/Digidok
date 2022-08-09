d6b298cdcc0490c64621b53eb8a33ff2230cb6b5
eril19 <85010681+eril19@users.noreply.github.com>
2022-08-09 12:23:56 +0700

    Update ProfileOptionActivity.kt

--- app/src/main/java/com/example/digidok/ProfileOptionActivity.kt
+++ app/src/main/java/com/example/digidok/ProfileOptionActivity.kt
@@ -2,12 +2,15 @@ package com.example.digidok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ProfileOptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_option)
 
-        val header = findViewById<>()
+        val header = findViewById<TextView>(R.id.header_title)
+
+        header.setText("Profile Optioms")
     }
 }
