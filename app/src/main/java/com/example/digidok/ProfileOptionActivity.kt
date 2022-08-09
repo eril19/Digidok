a2d8a7f494d03037700e724ec8e8a271703315c6
eril19 <85010681+eril19@users.noreply.github.com>
2022-08-09 12:16:51 +0700

    barokah

---
+++ app/src/main/java/com/example/digidok/ProfileOptionActivity.kt
@@ -0,0 +1,13 @@
+package com.example.digidok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileOptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_option)
+
+        val header = findViewById<>()
+    }
+}
