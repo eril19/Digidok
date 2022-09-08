fbf1be095d92d9b6d165b4a4cd414168a6c51dd3
rifdahdefrina <71772541+rifdahdefrina@users.noreply.github.com>
2022-09-08 15:15:57 +0700

    add details

---
+++ app/src/main/java/com/example/digidok/KjppDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.digidok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KjppDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kjpp_detail)
+    }
+}
---
+++ app/src/main/java/com/example/digidok/MitraDetailActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.digidok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MitraDetailActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mitra_detail2)
+    }
+}
