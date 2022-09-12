cb7f9766be0a73192dfa974f82c68eb33a3dcab8
rifdahdefrina <71772541+rifdahdefrina@users.noreply.github.com>
2022-09-12 20:41:42 +0700

    add laporan pengajuan

---
+++ app/src/main/java/com/example/digidok/LaporanPengajuanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.digidok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaporanPengajuanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_laporan_pengajuan)
+    }
+}
