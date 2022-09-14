56e040c487af73af26ad48477d0a5e56fe240c1b
rifdahdefrina <71772541+rifdahdefrina@users.noreply.github.com>
2022-09-14 09:33:45 +0700

    add cek dokumen

---
+++ app/src/main/java/com/example/digidok/CekDokumenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.digidok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CekDokumenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cek_dokumen)
+    }
+}
