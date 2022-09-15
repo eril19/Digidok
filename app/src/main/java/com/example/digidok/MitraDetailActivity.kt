bfdd379d8215a2b3ca25c491fe4483905a436429
rifdahdefrina <71772541+rifdahdefrina@users.noreply.github.com>
2022-09-15 16:21:43 +0700

    update layout mitra detail

--- app/src/main/java/com/example/digidok/MitraDetailActivity.kt
+++ app/src/main/java/com/example/digidok/MitraDetailActivity.kt
@@ -1,6 +1,9 @@
 package com.example.digidok
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,12 +15,56 @@ class MitraDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mitra_detail)
 
+        val next_detail_btn = findViewById<Button>(R.id.next_detail_btn)
+        next_detail_btn.setOnClickListener {
+            startActivity(Intent(this@MitraDetailActivity, MitraDetailActivity2::class.java))
+        }
+
+        val close_detail_btn = findViewById<Button>(R.id.close_detail_btn)
+        close_detail_btn.setOnClickListener {
+            startActivity(Intent(this@MitraDetailActivity, DaftarMitraActivity::class.java))
+        }
+
         data = intent.getParcelableExtra("daftarMitra")
         val npwp = findViewById<TextView>(R.id.npwp)
         val nama = findViewById<TextView>(R.id.nama)
+        val alamat = findViewById<TextView>(R.id.alamat)
+        val kelurahan = findViewById<TextView>(R.id.kelurahan)
+        val kecamatan = findViewById<TextView>(R.id.kecamatan)
+        val kota = findViewById<TextView>(R.id.kota)
+        val provinsi = findViewById<TextView>(R.id.provinsi)
+        val klasifikasi = findViewById<TextView>(R.id.klasifikasi)
+        val kpp = findViewById<TextView>(R.id.kpp)
+        val kanwil = findViewById<TextView>(R.id.kanwil)
+        val telp= findViewById<TextView>(R.id.telp_mitra)
+        val fax = findViewById<TextView>(R.id.fax_mitra)
+        val email = findViewById<TextView>(R.id.email_mitra)
+        val ttl = findViewById<TextView>(R.id.ttl_mitra)
+        val tgl_daftar = findViewById<TextView>(R.id.tgl_daftar_mitra)
+        val status_pkp= findViewById<TextView>(R.id.status_pkp_mitra)
+        val tgl_pkp = findViewById<TextView>(R.id.tgl_pkp_mitra)
+        val jenis_pajak = findViewById<TextView>(R.id.jenis_pajak_mitra)
+        val badan_hukum = findViewById<TextView>(R.id.badan_hukum_mitra)
+
 
         npwp.setText(data?.npwp)
         nama.setText(data?.nama_mitra)
-
+//        alamat.setText(data?.alamat_mitra)
+//        kelurahan.setText(data?.kelurahan_mitra)
+//        kecamatan.setText(data?.kecamatan_mitra)
+//        kota.setText(data?.kota_mitra)
+//        provinsi.setText(data?.provinsi_mitra)
+//        klasifikasi.setText(data?.klasifikasi_mitra)
+//        kpp.setText(data?.kpp_mitra)
+//        kanwil.setText(data?.kanwil_mitra)
+//        telp.setText(data?.telp_mitra)
+//        fax.setText(data?.fax_mitra)
+//        email.setText(data?.email_mitra)
+//        ttl.setText(data?.ttl_mitra)
+//        tgl_daftar.setText(data?.tgl_daftar_mitra)
+//        status_pkp.setText(data?.status_pkp_mitra)
+//        tgl_pkp.setText(data?.tgl_pkp_mitra)
+//        jenis_pajak.setText(data?.jeniw_pajak_mitra)
+//        badan_hukum.setText(data?.badan_hukum_mitra)
     }
 }
--- app/src/main/java/com/example/digidok/MitraDetailActivity2.kt
+++ app/src/main/java/com/example/digidok/MitraDetailActivity2.kt
@@ -1,11 +1,23 @@
 package com.example.digidok
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MitraDetailActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mitra_detail2)
+
+        val prev_detail_btn = findViewById<Button>(R.id.prev_detail_btn)
+        prev_detail_btn.setOnClickListener {
+            startActivity(Intent(this@MitraDetailActivity2, MitraDetailActivity::class.java))
+        }
+
+        val close_detail_btn = findViewById<Button>(R.id.close_detail_btn)
+        close_detail_btn.setOnClickListener {
+            startActivity(Intent(this@MitraDetailActivity2, DaftarMitraActivity::class.java))
+        }
     }
 }
